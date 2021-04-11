f1aa085c7de001170a1014f39e27167aba42a90c
masbossun <setiagi.ryan@gmail.com>
2021-04-11 14:08:36 +0700

    add custom fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,11 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@500&family=DM+Sans&family=Source+Serif+Pro:wght@900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <main></main>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ["./pages/**/**.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ['"DM Sans"', "sans-serif"],
+        title: ['"Source Serif Pro"', "serif"],
+        monospace: ['"DM Mono"', "monospace"],
+      },
+    },
   },
   variants: {
     extend: {},
