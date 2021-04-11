3ad2f0c0790e3f78073d1ca76f0980e3232940ae
masbossun <setiagi.ryan@gmail.com>
2021-04-11 14:23:29 +0700

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   purge: ["./pages/**/**.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
+    colors: {
+      "black-primary": "var(--color-black-primary)",
+      "black-secondary": "var(--color-black-secondary)",
+      "white-primary": "var(--color-white-primary)",
+      "white-secondary": "var(--color-white-secondary)",
+      "accent-primary": "var(--color-accent-primary)",
+      "accent-secondary": "var(--color-accent-secondary)",
+    },
     extend: {
       fontFamily: {
         body: ['"DM Sans"', "sans-serif"],
