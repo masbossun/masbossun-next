23e481a840bfc7fc742cdd55b3ee7c951c391c0c
masbossun <setiagi.ryan@gmail.com>
2021-04-13 06:54:18 +0700

    chore: lint set console as error

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   plugins: ["react", "@typescript-eslint"],
   rules: {
     "no-unused-vars": "warn",
+    "no-console": "error",
     "prettier/prettier": "error",
     "react/jsx-uses-react": "off",
     "react/react-in-jsx-scope": "off",
