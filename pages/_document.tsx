2de524714a95ee996b2d529faee32807c4578d54
masbossun <setiagi.ryan@gmail.com>
2021-05-05 07:56:49 +0700

    favicon: update favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,7 +16,24 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@500&family=DM+Sans&family=Source+Serif+Pro:wght@900&display=swap"
