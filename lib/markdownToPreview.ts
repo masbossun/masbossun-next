7cb0872e7cbd9893ce26669416a4efda549313fe
Ryan Setiagi <ryan@rukita.co>
2021-12-04 00:15:19 +0700

    feat: upgrade modules

--- lib/markdownToPreview.ts
+++ lib/markdownToPreview.ts
@@ -3,7 +3,9 @@ import html from "remark-html";
 const strip = require("remark-strip-html");
 
 export default async function markdownToPreview(markdown: string) {
-  const result = await remark().use(html).process(markdown);
+  const result = await remark()
+    .use(html as any)
+    .process(markdown);
   const preview = result.toString().match(/<p>(.*?)<\/p>/g);
   const strips = await remark()
     .use(strip)
