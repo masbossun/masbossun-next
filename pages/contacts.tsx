9df5a0bd2291ade168f9939e3ba92a63c5978f8f
Ryan Setiagi <ryan@rukita.co>
2021-12-08 20:07:33 +0700

    fix: contact pages

--- pages/contacts.tsx
+++ pages/contacts.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Navbar from "../components/navbar";
+import { Footer } from "../components/footer";
 
 const Contacts = () => {
   return (
@@ -7,10 +8,20 @@ const Contacts = () => {
       <Head>
         <title>contacts — masbossun.dev</title>
       </Head>
-      email: setiagi.ryan@gmail.com
       <header>
         <Navbar />
       </header>
+
+      <main style={{ minHeight: "50vh" }}>
+        <h1 className="text-black-primary dark:text-white-primary my-20 md:my-40 max-w-page-title">
+          Keep in touch
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <span>email: setiagi.ryan@gmail.com</span>
+        </div>
+      </main>
+
+      <Footer />
     </>
   );
 };
